ec37ff59af7b5e422c77343ce071462c9d5b7cd7
NIJAM SAYYAD <58353284+NKS2900@users.noreply.github.com>
2020-11-19 09:23:52 +0530

    Added: PinCode Validation .

--- RegEx.java
+++ RegEx.java
@@ -4,26 +4,25 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegExLastName {
+public class RegExPinCode {
 
 	static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Enter Last_Name : ");
-		String lname = scan.nextLine();
-
-		boolean b = isValidPassword(lname);
-		if (b)
-			System.out.println("Strong_Password!!!");
+		System.out.println("Enter Pin_Code : ");
+		String pin = scan.nextLine();
+		boolean b=isValid(pin);
+		if(b)
+			System.out.println("Vlaid_Pin_Code!!!");
 		else
-			System.out.println("Invalid_Password???");
+			System.out.println("Invalid_Pin_Code.???");
 	}
-	public static boolean isValidPassword(String lname) {
 
-		String regex = "^([A-Z]{1})+[a-zA-z0-9]{2,}$";
+	public static boolean isValid(String pin) {
+		String regex = "^[0-9]{6}$";
 		Pattern p = Pattern.compile(regex);
-		Matcher m = p.matcher(lname);
+		Matcher m = p.matcher(pin);
 		return m.matches();
 	}
 }
