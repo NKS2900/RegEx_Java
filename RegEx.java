782b21c70c2606e4463d299c298978b31d2d1877
NIJAM SAYYAD <58353284+NKS2900@users.noreply.github.com>
2020-11-19 10:17:11 +0530

    Added: E-mail validation.

--- RegEx.java
+++ RegEx.java
@@ -4,25 +4,27 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegExPinCode {
+public class RegEx {
 
 	static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Enter Pin_Code : ");
-		String pin = scan.nextLine();
-		boolean b=isValid(pin);
-		if(b)
-			System.out.println("Vlaid_Pin_Code!!!");
-		else
-			System.out.println("Invalid_Pin_Code.???");
+		do {
+			System.out.println("Enter E-mail : ");
+			String email = scan.nextLine();
+			boolean b = isValid(email);
+			if (b)
+				System.out.println(email + " -->is_Vlaid_Email...");
+			else
+				System.out.println("Invalid_Email.???");
+		} while (true);
 	}
-
-	public static boolean isValid(String pin) {
-		String regex = "^[0-9]{6}$";
+	
+	public static boolean isValid(String email) {
+		String regex = "^[a-zA-Z0-9]+[.(a-zA-Z0-9)]*(\\@)[a-zA-Z0-9]+(\\.)[a-z]{2,4}$";
 		Pattern p = Pattern.compile(regex);
-		Matcher m = p.matcher(pin);
+		Matcher m = p.matcher(email);
 		return m.matches();
 	}
 }
